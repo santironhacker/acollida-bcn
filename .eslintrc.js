38903fcb7457d5bdccb74cdd3f1c19f07ab7ce63
Santiago Ramos <santiago.ramos.romeu@everis.com>
2019-10-12 18:18:42 +0200

    added eslintrc and configured rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,14 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ['plugin:vue/essential', '@vue/prettier'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/require-v-for-key': 'false',
+    'vetur.validation.vue-html': 'false'
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   }
 };
