e8586ede497cf38850e48aedbad3a8ea7411521d
Santiago Ramos <santiago.ramos.romeu@everis.com>
2019-10-12 18:19:08 +0200

    creater header and footer componetns

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,50 @@
+<template>
+  <b-container class="p-2" id="back-color">
+    <b-row class="text-center">
+      <b-col>
+        <span>Segueix-nos a:</span>
+        <a
+          class="whited"
+          target="_blank"
+          href="https://www.facebook.com/AcollidaTaizeBarcelona/"
+          rel="noopener noreferrer"
+        >
+          <font-awesome-icon class="ml-4" :icon="['fab', 'facebook-square']" />
+        </a>
+        <a
+          class="whited"
+          target="_blank"
+          href="https://www.instagram.com/acollidabcn/"
+          rel="noopener noreferrer"
+        >
+          <font-awesome-icon class="ml-4" :icon="['fab', 'instagram']" />
+        </a>
+        <a
+          class="whited"
+          target="_blank"
+          href="https://twitter.com/AcollidaBCN"
+          rel="noopener noreferrer"
+        >
+          <font-awesome-icon class="ml-4" :icon="['fab', 'twitter']" />
+        </a>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter'
+};
+</script>
+
+<style scoped>
+a :hover {
+  cursor: pointer;
+  color: black;
+}
+
+.whited {
+  color: white;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,85 @@
+<template>
+  <b-container fluid style="max-width: 100%">
+    <b-row id="back-color">
+      <b-col class="p-3 header-title title">
+        <span>Acollida de Barcelona</span>
+      </b-col>
+      <b-navbar-toggle target="nav-collapse">
+        <b-button class="p-1,5 header-button">
+          <font-awesome-icon class="mr-2" icon="bars" />
+          <span>MENÚ</span>
+        </b-button>
+      </b-navbar-toggle>
+    </b-row>
+
+    <b-row>
+      <b-col>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar class="nav-dropdown-items float-right" toggleable="lg" type="light">
+            <b-img
+              class="m-2"
+              :src="require('../assets/img/logo-acollida.png')"
+              fluid
+              alt="Logo acollida Barcelona"
+              style="height: 6rem"
+            ></b-img>
+            <b-navbar-nav>
+              <b-dropdown-item>
+                <router-link to="/home">Inici</router-link>
+              </b-dropdown-item>
+              <b-dropdown-item>
+                <router-link to="/about">Qui som</router-link>
+              </b-dropdown-item>
+              <b-dropdown-item>
+                <router-link to="/campaigns">Campanyes</router-link>
+              </b-dropdown-item>
+              <b-dropdown-item>
+                <router-link to="/prayers">Pregàries</router-link>
+              </b-dropdown-item>
+            </b-navbar-nav>
+          </b-navbar>
+        </b-collapse>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  props: {},
+  data: function() {
+    return {
+      ex: 'This message',
+      isTod: true
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.nav-dropdown-items {
+  border: solid 1px black;
+  margin-top: 1rem;
+}
+
+.nav-dropdown-items a {
+  color: rgba(20, 19, 19, 0.842);
+}
+
+.nav-dropdown-items a.router-link-exact-active {
+  color: #c0470b;
+}
+
+.header-title {
+  text-transform: uppercase;
+  color: white;
+}
+
+.header-button {
+  color: #c0470b !important;
+  background: white !important;
+  font-weight: 600;
+}
+</style>
