62546ad2f9616224ded26340ac53282698c8dea6
Santiago Ramos <santiago.ramos.romeu@everis.com>
2019-10-26 15:30:37 +0200

    added for into label name

--- src/views/public/Home.vue
+++ src/views/public/Home.vue
@@ -3,9 +3,7 @@
     <b-container fluid style="max-width: 100%">
       <b-row>
         <b-col>
-          <div class="pt-2 pb-2 title">
-            Punt de trobada entre Taizé i Barcelona
-          </div>
+          <div class="pt-2 pb-2 title">Punt de trobada entre Taizé i Barcelona</div>
         </b-col>
       </b-row>
       <b-row>
@@ -57,6 +55,7 @@
           <form class="mt-2">
             <div class="form-group">
               <label
+                for="name"
                 class="control-label"
                 :class="
                   $v.form.name.$error
@@ -65,8 +64,7 @@
                     ? 'text-success'
                     : ''
                 "
-                >Nom: *</label
-              >
+              >Nom: *</label>
               <input
                 class="form-control"
                 :class="
@@ -83,9 +81,7 @@
               <div
                 class="help-block error mt-1"
                 v-if="!$v.form.name.required && $v.form.name.$error"
-              >
-                Camp obligatori
-              </div>
+              >Camp obligatori</div>
               <div
                 class="help-block error mt-1"
                 v-if="!$v.form.name.minLength && $v.form.name.$error"
@@ -106,8 +102,7 @@
                     ? 'text-success'
                     : ''
                 "
-                >Cognoms: *</label
-              >
+              >Cognoms: *</label>
               <input
                 class="form-control"
                 :class="
@@ -124,9 +119,7 @@
               <div
                 class="help-block error mt-1"
                 v-if="!$v.form.surname.required && $v.form.surname.$error"
-              >
-                Camp obligatori
-              </div>
+              >Camp obligatori</div>
               <div
                 class="help-block error mt-1"
                 v-if="!$v.form.surname.minLength && $v.form.surname.$error"
@@ -147,8 +140,7 @@
                     ? 'text-success'
                     : ''
                 "
-                >Correu electrònic: *</label
-              >
+              >Correu electrònic: *</label>
               <input
                 class="form-control"
                 :class="
@@ -165,9 +157,7 @@
               <div
                 class="help-block error mt-1"
                 v-if="!$v.form.email.required && $v.form.email.$error"
-              >
-                Camp obligatori
-              </div>
+              >Camp obligatori</div>
             </div>
 
             <!-- <div>
@@ -179,20 +169,13 @@
             </div>-->
 
             <div class="form-group mt-3">
-              <label class="control-label" for="hearFromUs"
-                >Com ens has conegut?</label
-              >
-              <select
-                class="form-control"
-                id="hearFromUs"
-                v-model="form.hearFromUs"
-              >
+              <label class="control-label" for="hearFromUs">Com ens has conegut?</label>
+              <select class="form-control" id="hearFromUs" v-model="form.hearFromUs">
                 <option
                   v-for="option in hearFromUsOptions"
                   :key="option"
                   style="font-family: Ubuntu, font-size: 10px, overflow: none"
-                  >{{ option }}</option
-                >
+                >{{ option }}</option>
               </select>
             </div>
 
@@ -207,13 +190,8 @@
                       : 'text-danger'
                     : ''
                 "
-                >Accepto les condicions</label
-              >
-              <input
-                type="checkbox"
-                id="accept"
-                v-model="$v.form.accept.$model"
-              />
+              >Accepto les condicions</label>
+              <input type="checkbox" id="accept" v-model="$v.form.accept.$model" />
             </div>
 
             <b-button class="submit" id="back-color" @click.prevent="submit">
@@ -224,22 +202,18 @@
                 El formulari conté errors,
                 <br />siusplau dona-li un cop d'ull.
               </p>
-              <p
-                v-else-if="formTouched && uiState === 'submit clicked'"
-                class="text-warning"
-              >
+              <p v-else-if="formTouched && uiState === 'submit clicked'" class="text-warning">
                 El formulari és buit,
                 <br />siusplau omple el formulari per suscriure't!
               </p>
-              <p v-else-if="uiState === 'form submitted'" class="text-success">
-                Formulari enviat correctament!
-              </p>
+              <p
+                v-else-if="uiState === 'form submitted'"
+                class="text-success"
+              >Formulari enviat correctament!</p>
               <p
                 v-else-if="uiState === 'form returns error'"
                 class="text-warning"
-              >
-                Error en processar la petició. Provi més tard.
-              </p>
+              >Error en processar la petició. Provi més tard.</p>
             </div>
           </form>
         </b-col>
@@ -318,7 +292,6 @@ export default {
         this.formTouched === false &&
         this.form.accept === true
       ) {
-        //this is where you send the responses
         /* const key = db
           .ref()
           .child('newsletter')
@@ -346,14 +319,10 @@ export default {
               this.uiState = 'form returns error';
             }
           ); */
+        //this is where you send the responses
         const key = db
           .collection('newsletter')
           .doc(this.form.email)
-          
-          /* .push().key;
-        db.ref()
-          .child('newsletter')
-          .child(key) */
           .set({
             name: this.form.name,
             surname: this.form.surname,
