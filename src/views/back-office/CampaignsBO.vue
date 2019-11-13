c002daaabd1c1d1007971df2549c9ddd1403a21e
Santi Ramos <santi92.network@gmail.com>
2019-11-13 17:46:30 +0100

    revised call to DB to retrieve available campaigns. Created a mock object. Displayed with v-for. Merged id with object data

--- src/views/back-office/CampaignsBO.vue
+++ src/views/back-office/CampaignsBO.vue
@@ -24,22 +24,25 @@
           <section v-else>
             <div v-if="loading">Carregant ...</div>
             <div v-else>
-              <div v-if="campaigns">
-                {{ campaigns }}
-                <!-- <campaign-list-item
-                  v-for="(item, index) in openCampaigns"
+              <div v-if="campaigns.length > 0">
+                <div
+                  v-for="(campaign, index) in campaigns"
                   :key="index"
-                  :item="item"
-                ></campaign-list-item> -->
+                >
+                  <div>
+                    {{ campaign.id }}
+                  </div>
+                  <div>
+                    {{ campaign.title }}
+                  </div>
+                </div>
               </div>
               <div v-else>
-                <div>
-                  No hi ha campanyes. Per començar, crea una nova campanya.
-                </div> 
+                No hi ha campanyes. Per començar, crea una nova campanya.
+              </div>
                 <b-button class="mt-2" type="button" id="back-color" @click="addCampaign">
                   <span>Crea una nova campanya</span>
                 </b-button>
-              </div>
             </div>
           </section>
           
@@ -52,7 +55,6 @@
 </template>
 
 <script>
-// import { auth } from '../../main';
 import { db } from '../../main';
 import router from '../../router';
 
@@ -60,7 +62,8 @@ export default {
   name: 'campaignsBO',
   data() {
     return {
-      campaigns: null,
+      campaigns: [],
+      campaignsIds: [],
       loading: true,
       errored: false
     };
@@ -71,17 +74,23 @@ export default {
     }
   },
   mounted() {
-    db.collection('campaigns').orderBy('uid').limit(3).get()
+    const self = this;
+    // DB Call
+    // .orderBy('uid')
+    db.collection('campaigns').limit(2).get()
       .then(snapshot => {
         console.log('snapshot', snapshot)
         if(snapshot.empty) {
           console.log('No matching campaigns');
           return;
         } else {
-          snapshot.forEach(doc => {
+          snapshot.docs.forEach(doc => {
             console.log(doc.id, '=>', doc.data());
+            const idObject = { id: doc.id }
+            const dataObject = doc.data();
+            const toPushObject = Object.assign(dataObject, idObject);
+            self.campaigns.push(toPushObject);
           });
-          this.campaigns = snapshot.docs;
         }
       })
       .catch(err => {
@@ -89,6 +98,41 @@ export default {
         this.errored = true;
       })
       .finally(() => this.loading = false)
+
+      // MOCK Call
+      /* const mockCampaignsArray = [
+        {
+          id: HftFwqBPbLmG70A1fjDf,
+          buses: [],
+          createdBy: null,
+          endDate: {
+            nanoseconds: 0,
+            seconds: 1574290800
+          },
+          startDate: {
+            nanoseconds: 0,
+            seconds: 1573686000
+          },
+          subscriptionsStatus: 'Inscripcions obertes',
+          title: 'Estiu 2019',
+          weeks: []
+        },
+        {
+          id: HvkCw71omWM52eHXPVIV,
+          buses: [],
+          createdBy: null,
+          endDate: null,
+          startDate: null,
+          subscriptionsStatus: 'Llista d\'espera',
+          title: 'Setmana Santa 2020',
+          weeks: []
+        }
+      ];
+      mockCampaignsArray.forEach(
+        item => {
+          self.campaigns.push(item);
+        }
+      ); */
   }
 };
 </script>
