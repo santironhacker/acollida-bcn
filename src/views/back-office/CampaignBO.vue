26c585d89cb3fee99958dca06714cd37fff60397
Santi Ramos <santi92.network@gmail.com>
2019-12-03 12:04:42 +0100

    added basic info display for the campaign detail

--- src/views/back-office/CampaignBO.vue
+++ src/views/back-office/CampaignBO.vue
@@ -2,16 +2,35 @@
   <div>
     <b-container fluid>
       <b-row>
-        <b-col>
+        <b-col cols="8">
           <h2 class="title pt-3 pb-2">Campanya BO</h2>
-          <h2 class="title pt-3 pb-2"> xxx </h2>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col>
+          <div>
+            Benvingut al menú d'edició de la campanya. Aquí podràs editar totes les questions relatives a la campanya, així com visualitzar els busos y usuaris inscrits.
+          </div>
         </b-col>
       </b-row>
 
       <b-row class="mt-3 mb-3">
         <b-col>
-          <h4 class="title">Detalls de la campanya</h4>
-          
+          <div class="title">Detalls de la campanya</div>
+          <div>
+            Títol:
+            {{ campaign.title }}
+          </div>
+          <div>
+            Dates:
+            del {{ campaign.startDate | formatDate }} al {{ campaign.endDate | formatDate }}
+          </div>
+          <div>
+            Creada per:
+            {{ campaign.createdBy }}
+          </div>
+          <!-- <h2 class="title pt-3 pb-2"></h2> -->
         </b-col>
       </b-row>
 
@@ -24,36 +43,51 @@
 // import { auth } from '../../main';
 import { db } from '../../main';
 import router from '../../router';
+import { useMockData } from '../../main';
 
 export default {
   name: 'campaignBO',
   data() {
     return {
-        
+        campaign: {}
     };
   },
   methods: {
     
   },
   mounted() {
-    /* db.collection('campaigns').orderBy('uid').limit(3).get()
-      .then(snapshot => {
-        console.log('snapshot', snapshot)
-        if(snapshot.empty) {
-          console.log('No matching campaigns');
-          return;
-        } else {
-          snapshot.forEach(doc => {
-            console.log(doc.id, '=>', doc.data());
-          });
-          this.campaigns = snapshot.docs;
-        }
-      })
-      .catch(err => {
-        console.log('Error getting documents', err);
-        this.errored = true;
-      })
-      .finally(() => this.loading = false) */
+    if(!useMockData) {
+      /* db.collection('campaigns').orderBy('uid').limit(3).get()
+        .then(snapshot => {
+          console.log('snapshot', snapshot)
+          if(snapshot.empty) {
+            console.log('No matching campaigns');
+            return;
+          } else {
+            snapshot.forEach(doc => {
+              console.log(doc.id, '=>', doc.data());
+            });
+            this.campaigns = snapshot.docs;
+          }
+        })
+        .catch(err => {
+          console.log('Error getting documents', err);
+          this.errored = true;
+        })
+        .finally(() => this.loading = false) */
+    } else {
+      this.campaign = {
+        id: 'HftFwqBPbLmG70A1fjDf',
+        buses: [],
+        createdBy: 'santi92.network@gmail.com',
+        endDate: new Date('2020-09-06T23:59:59'),
+        startDate: new Date('2020-06-27T00:00:00'),
+        subscriptionsStatus: 'Inscripcions obertes',
+        title: 'Estiu 2020',
+        weeks: [],
+        isPublished: false
+      }
+    }
   }
 };
 </script>
